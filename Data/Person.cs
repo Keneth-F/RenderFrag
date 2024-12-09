8ce03b872a6f9c01c9cbf6d4d012ff8bac926c80
Keneth-F <kenethfg123@gmail.com>
2024-12-09 14:47:39 -0600

    Agregar archivos de proyecto.

---
+++ Data/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace RenderFrag.Data
+{
+    public class Person
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public List<Course> Courses { get; set; }
+    }
+}
